1d196310f3e2f3a4aa4a040ec4f5211a9d09578a
harrypango <harry.pango@gmail.com>
2024-02-20 00:00:19 +0100

    added mobile responsiveness

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -4,7 +4,7 @@ const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
   return (
     <div
-      className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+      className="flex flex-col h-[220px] xl:h-auto w-[150px] xl:w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
       onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
     >
       <img
@@ -12,7 +12,7 @@ const ArtistCard = ({ track }) => {
         src={track?.images?.coverart}
         className="w-full h-56 rounded-lg"
       />
-      <p className="mt-4 font-semibold text-lg text-white truncate">
+      <p className="mt-4 pb-4 font-semibold text-lg text-white truncate">
         {track?.subtitle}
       </p>
     </div>
